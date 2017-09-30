e50ba6ccd76cd3a2d4904a2d6a65cb8ed5c043dc
Sam Echikson <sechikson@mbp-sechikson.attlocal.net>
2017-09-30 16:42:51 -0400

    Add SVGs for jack, queen and king ranks

--- src/playingCard.js
+++ src/playingCard.js
@@ -3,9 +3,13 @@ import SpadeSVG from './svg/spade.svg';
 import HeartSVG from './svg/heart.svg';
 import ClubsSVG from './svg/club.svg';
 import DiamondSVG from './svg/diamond.svg';
-import JackSVG from './svg/jack.svg';
+import JackBlackSVG from './svg/jackBlack.svg';
+import JackRedSVG from './svg/jackRed.svg';
+import QueenBlackSVG from './svg/queenBlack.svg';
+import QueenRedSVG from './svg/queenRed.svg';
+import KingBlackSVG from './svg/kingBlack.svg';
+import KingRedSVG from './svg/kingRed.svg';
 
-import { Card } from 'material-ui/Card';
 import './playingCard.css';
 
 export default class PlayingCard extends Component {
@@ -17,9 +21,18 @@ export default class PlayingCard extends Component {
             suit === 'diamonds' ? (<DiamondSVG />) : null;
     }
 
+    renderRank(card) {
+        return card.getRank() === 'jack' && ['clubs', 'spades'].includes(card.getSuit()) ? (<JackBlackSVG />) :
+            card.getRank() === 'jack' && ['hearts', 'diamonds'].includes(card.getSuit()) ? (<JackRedSVG />) : 
+            card.getRank() === 'queen' && ['clubs', 'spades'].includes(card.getSuit()) ? (<QueenBlackSVG />) : 
+            card.getRank() === 'queen' && ['hearts', 'diamonds'].includes(card.getSuit()) ? (<QueenRedSVG />): 
+            card.getRank() === 'king' && ['clubs', 'spades'].includes(card.getSuit()) ? (<KingBlackSVG />) :
+            card.getRank() === 'king' && ['hearts', 'diamonds'].includes(card.getSuit()) ? (<KingRedSVG />) : null;
+    }
+
     render() {
         return (
-            <div className="Card">
+            <div className={"Card " + this.props.card.getSuit()}>
                 <div className="Panel Top">
                     <div className="Rank">
                         {this.props.card.getRank()}
@@ -32,7 +45,7 @@ export default class PlayingCard extends Component {
                     </div>
                 </div>
                 <div className="Content">
-                    <div><JackSVG /></div>
+                    {this.renderRank(this.props.card)}
                 </div>
                 <div className="Panel Bottom">
                 </div>
